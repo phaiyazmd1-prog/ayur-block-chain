f3793e1e8f61446f8ae64e74d55d137204c33471
phaiyaz <sekhphaiyaz@gmail.com>
2025-09-18 23:42:26 +0545

    first commit

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -46,7 +46,7 @@ const Navigation: React.FC<NavigationProps> = ({ currentView, onViewChange }) =>
                 <Leaf className="w-5 h-5 text-white" />
               </div>
               <span className="text-xl font-bold bg-herb-gradient bg-clip-text text-transparent">
-                HerbTrace
+                Herbal
               </span>
             </motion.div>
 
